cfa0f80203849b110d9e2a013f9fe74bbcd26d71
Eiko Smid <smid.eiko@gmail.com>
2026-02-12 14:54:18 +0100

    Added the table 'users' as new table into the DB. THis tables will store the user data (id, email, hashed_pwd, is_active state and the role of the users).

--- src/db/table_definitions/01_schema.sql
+++ src/db/table_definitions/01_schema.sql
@@ -3,12 +3,15 @@
 -- if yes all files inside "/docker-entrypoint-initdb.d/" will be run automatically in alphabetic order.
 -- Use same column names as CSV headers to simplify COPY. Put special ones like "movieId" in quotes 
 -- otherwise postgres will automatically lower case them to: movieid and then it ma yenter problem in code.
+
+-- # Create movies table to store movie info
 CREATE TABLE IF NOT EXISTS movies (
     "movieId"   INTEGER PRIMARY KEY,
     title       TEXT NOT NULL,
     genres      TEXT NOT NULL
 );
 
+-- # Create ratings table to store ratings info
 CREATE TABLE IF NOT EXISTS ratings (
     "userId"    INTEGER NOT NULL,
     "movieId"   INTEGER NOT NULL REFERENCES movies("movieId") ON DELETE CASCADE,
@@ -16,10 +19,30 @@ CREATE TABLE IF NOT EXISTS ratings (
     "timestamp" BIGINT NOT NULL  -- quoted because it's a reserved word
 );
 
+-- Create type to ensure our column later on can only contain the predefined roles. 
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
+        CREATE TYPE user_role AS ENUM ('USER', 'DEVELOPER', 'ADMIN');
+    END IF;
+END$$;
+
+-- Create user authentication and authorization table
+CREATE TABLE IF NOT EXISTS users (
+    id              BIGSERIAL PRIMARY KEY,          -- SQLAlchemy int PK; BIGSERIAL is common in Postgres
+    email           VARCHAR(320) NOT NULL UNIQUE,    -- same 320 length, unique, not null
+    hashed_password VARCHAR(255) NOT NULL,           -- same 255 length, not null
+    is_active       BOOLEAN NOT NULL DEFAULT TRUE,   -- default True, not null
+    role            user_role NOT NULL DEFAULT 'USER'-- default USER
+);
+
+
+-- Create app_meta tabel which later be used to check if copy process of data is finished -> start api
 CREATE TABLE IF NOT EXISTS app_meta(
     key TEXT Primary Key,
     value TEXT NOT NULL
 );
+
 
 -- Helpful indexes
 CREATE INDEX IF NOT EXISTS idx_ratings_user ON ratings("userId");
