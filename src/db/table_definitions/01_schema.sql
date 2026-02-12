d2fbe0921b8d8ac839d65ed8cf26ee8ac69da9a3
Eiko Smid <smid.eiko@gmail.com>
2026-02-12 13:02:52 +0100

    Moved the table creation scripts into src/db and upüdated compose file. Changed the healtheck of the DB in Docker compose. Now a table will be generated and if its value is true, the healthcheck is complete. This is only the case after the tables have been created and all data has been copied. Only after that the api starts.

---
+++ src/db/table_definitions/01_schema.sql
@@ -0,0 +1,26 @@
+-- Inits the Database. 
+-- Gets called automnatically. The containers build in logic checks if "PGDATA" folder is empty
+-- if yes all files inside "/docker-entrypoint-initdb.d/" will be run automatically in alphabetic order.
+-- Use same column names as CSV headers to simplify COPY. Put special ones like "movieId" in quotes 
+-- otherwise postgres will automatically lower case them to: movieid and then it ma yenter problem in code.
+CREATE TABLE IF NOT EXISTS movies (
+    "movieId"   INTEGER PRIMARY KEY,
+    title       TEXT NOT NULL,
+    genres      TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ratings (
+    "userId"    INTEGER NOT NULL,
+    "movieId"   INTEGER NOT NULL REFERENCES movies("movieId") ON DELETE CASCADE,
+    rating      NUMERIC(2,1) NOT NULL,
+    "timestamp" BIGINT NOT NULL  -- quoted because it's a reserved word
+);
+
+CREATE TABLE IF NOT EXISTS app_meta(
+    key TEXT Primary Key,
+    value TEXT NOT NULL
+);
+
+-- Helpful indexes
+CREATE INDEX IF NOT EXISTS idx_ratings_user ON ratings("userId");
+CREATE INDEX IF NOT EXISTS idx_ratings_movie ON ratings("movieId");
---
+++ src/db/table_definitions/02_seed_data.sql
@@ -0,0 +1,20 @@
+-- postgres/initdb/03_seed_data.sql
+-- WARNING: runs only on first DB init (empty PGDATA).
+
+-- The CSVs will be mounted at /docker-entrypoint-initdb.d/seed
+\echo 'Seeding movies...'
+COPY public.movies("movieId", title, genres)
+FROM '/seed/movies.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"');
+
+\echo 'Seeding ratings...'
+COPY public.ratings("userId", "movieId", rating, "timestamp")
+FROM '/seed/ratings.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"');
+
+SELECT COUNT(*) FROM "movies";
+SELECT COUNT(*) FROM "ratings";
+
+INSERT INTO app_meta(key, value)
+VALUES ('seed_done', 'true')
+ON CONFLICT (key) DO UPDATE SET value = EXCLUDED.value;
