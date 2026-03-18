b6692b48cf2937a29bc401967b4f8f98607d3575
Eiko Smid <smid.eiko@gmail.com>
2026-03-18 15:41:22 +0100

    Mlflow is now only runned if environemnt var 'TESTING' is false. Everytime pytest starts, a fake DB is created by sql light. Test data is loaded in this DB automatically then and we are using this DB as a lightweight DB, to test our code.

--- tests/test_api/test_docs.py
+++ tests/test_api/test_docs.py
@@ -1,11 +1,10 @@
 from fastapi import status
-from fastapi.testclient import TestClient
 from src.api.main import app
 
-client = TestClient(app)
 
-
-def test_root_endpoint():
+def test_root_endpoint(client):
     '''Test API docs endpoint to ensure api is actually running.'''
     response = client.get("/docs")
     assert response.status_code == status.HTTP_200_OK
+
+    
--- tests/utils/db_test_movies.py
+++ tests/utils/db_test_movies.py
@@ -3,17 +3,17 @@ from src.db.models.movies import Movie
 MOVIE_1 = Movie(
     movieId=1,
     title="Test Movie 1",
-    genre="Action"
+    genres="Action"
 )
 
 MOVIE_2 = Movie(
     movieId=2,
     title="Test Movie 2",
-    genre="Comedy",
+    genres="Comedy",
 )
 
 MOVIE_3 = Movie(
     movieId=3,
     title="Test Movie 3",
-    genre="Drama",
+    genres="Drama",
 )
