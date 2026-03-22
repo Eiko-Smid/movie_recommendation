a3a8bcc456a68b7e0450251c2a901f9bb1d8f37c
Eiko Smid <smid.eiko@gmail.com>
2026-03-22 19:43:53 +0100

    Added network test into bash script to validate that network actually works.

--- .github/workflows/run_api_and_streamlit_container.sh
+++ .github/workflows/run_api_and_streamlit_container.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-# Define container name
+# Define API params
 CONTAINER_NAME_API="movie-reco-api-ci"
 IMAGE_NAME_API="movie-reco-api:ci"
 HEALTH_ENDPOINT_API="http://localhost:8000/health"
+INTERNAL_HEALTH_ENDPOINT_API="http://api:8000/health"
 
+# Define stramlit params
 CONTAINER_NAME_STREAMLIT="movie-reco-streamlit-ci"
 IMAGE_NAME_STREAMLIT="movie-reco-streamlit:ci"
 URL_STREAMLIT="http://localhost:8501"
 
+# Define DB 
 DB_URL="sqlite:///./test.db"
 
-NETWORK_NAME=""movie-rco-ci-net
+# Define docker network
+NETWORK_NAME="movie-rco-ci-net"
+
+# Define Summarization vars
+API_OK=false
+STREAMLIT_OK=false
+STREAMLIT_CAN_REACH_API=false
 
 echo "Removing old containers and networks if exists"
 docker rm -f $CONTAINER_NAME_API 2>/dev/null
@@ -37,7 +46,7 @@ docker run -d \
 docker run -d \
   --name $CONTAINER_NAME_STREAMLIT \
   --network $NETWORK_NAME \
-  -e DB_URL=$DB_URL \
+  -e DB_URL="$DB_URL" \
   -e API_URL="http://api:8000" \
   -p 8501:8501 \
   $IMAGE_NAME_STREAMLIT
@@ -59,6 +68,7 @@ echo "Testing if api health endpoint is reachable"
 for i in {1..10}; do
   if curl -s $HEALTH_ENDPOINT_API; then
     echo -e "\n\nHealth endpoint reachable!\n"
+    API_OK=true
     break
   fi
   echo "Waiting..."
@@ -69,15 +79,45 @@ echo "Testing if streamlit webserver is reachable"
 for i in {1..10}; do
   if curl -s $URL_STREAMLIT; then
     echo -e "\n\nStreamlit web app reachable!\n"
+    STREAMLIT_OK=true
     break
   fi
   echo "Waiting for Streamlit web app to start..."
   sleep 2
 done
 
+
+echo "Check if streamlit can reach API health endpoint"
+if docker exec "$CONTAINER_NAME_STREAMLIT" \
+  python -c "import requests; \
+requests.get('$INTERNAL_HEALTH_ENDPOINT_API', timeout=5).raise_for_status()"
+then
+  echo "Streamlit can reach api health endpoint."
+  STREAMLIT_CAN_REACH_API=true
+fi
+
 echo "Stop containers and network"
 docker stop $CONTAINER_NAME_API
 docker stop $CONTAINER_NAME_STREAMLIT
 docker network rm $NETWORK_NAME 2>/dev/null
+
+echo -e "\nSummarize results:\n"
+if [ "$API_OK" = true ]; then 
+  echo "API container run success!"
+else 
+  echo "API container run failed!"
+fi
+
+if [ "$STREAMLIT_OK" = true ]; then
+  echo "Streamlit container run success"
+else 
+  echo "Streamlit container run failed!"
+fi
+
+if [ "$STREAMLIT_CAN_REACH_API" == true ]; then
+  echo "Streamlit can reach api health endpoint."
+else
+  echo "Streamlit can't reach api health endpoint."
+fi
 
 echo "Done!"
